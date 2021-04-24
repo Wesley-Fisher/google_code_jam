4f83a6db205c9eb243636009117658d9263318ab
Wesley Fisher <wesley_b_fisher@hotmail.com>
2021-04-24 15:53:30 -0300

    Undo header add
    - Use one cpp file that can be copied

--- 00_default/problem.cpp
+++ 00_default/problem.cpp
@@ -1,4 +1,48 @@
-#include "problem.h"
+#ifdef TEST
+// https://google.github.io/googletest/quickstart-cmake.html
+#include <gtest/gtest.h>
+#endif
+
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+#include <vector>
+#include <string>
+#include <sstream>
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Define Utilities
+#define iow(w) std::setw(w)
+#define iop(p) std::setprecision(p)
+#define iowp(w,p) iow(w) << iop(p)
+
+#ifdef LOCAL
+  #define LOG(...) std::cout << __VA_ARGS__ << std::endl
+#else
+  #define LOG(...) 
+#endif
+
+bool OpenTest(std::string& name, std::ifstream& fin)
+{
+  std::string filename = "tests/" + name + ".txt";
+  fin.open(filename);
+  return fin.is_open();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Define common structures and functions
+struct CaseDetails;
+struct CaseSolution;
+
+void SolveProblem(std::istream& in, std::ostream& out);
+CaseDetails ReadCaseDetails(std::istream& in);
+CaseSolution SolveCase(CaseDetails details);
+void OutputSolution(CaseSolution solution, std::ostream& out, int case_num);
 
 
 ////////////////////////////////////////////////////////////////////////////////
