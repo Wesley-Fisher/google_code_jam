7832028d18b6795d466ab1e839a10155db680073
Wesley Fisher <wesley_b_fisher@hotmail.com>
2023-02-25 14:32:03 +0100

    Refactor shared function locations

--- 00_default_standard/problem.cpp
+++ 00_default_standard/problem.cpp
@@ -16,16 +16,6 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-// Problem-Specific Structs
-struct CaseDetails {
-};
-
-struct CaseSolution {
-};
-
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 // Main Function
 
 #ifndef NOMAIN
@@ -44,62 +34,32 @@ int main(int argc, char** argv)
 #endif
 
 
-void SolveProblem(std::istream& in, std::ostream& out) {
-    int num_problems = 0;
-    in >> num_problems;
-
-    for(int i=0; i<num_problems; i++) {
-        CaseDetails case_details = ReadCaseDetails(in);
-        CaseSolution solution = SolveCase(case_details, std::to_string(i+1));
-        OutputSolution(solution, out, i+1);
-    }
-}
-
-void AnalyzeProblem(std::istream& in, std::istream& answers) {
-    int num_problems = 0;
-    in >> num_problems;
-
-    LOG("ANALYZING " << num_problems << " problems");
-
-    for(int i=0; i<num_problems; i++) {
-        CaseDetails case_details = ReadCaseDetails(in);
-        CaseSolution solution = SolveCase(case_details, std::to_string(i+1));
-        
-        std::string ans = CaseOutput(solution, i+1);
-        ans.pop_back(); // Remove newline for logging.comparing
-        
-        std::string key;
-        std::getline(answers, key);
-        
-        if(!OutputCompare(ans, key))
-        {
-          LOG("MISMATCH (mine, key)");
-          LOG(ans);
-          LOG(key);
-          LOG("");
-        }
-    }
-}
-
-
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 // Problem-Specifics
 
-std::string CaseOutput(CaseSolution solution, int case_num)
+struct CaseDetails {
+};
+
+struct CaseSolution {
+};
+
+CaseDetails* ReadCaseDetails(std::istream& in)
+{
+  CaseDetails* details = new CaseDetails();
+  return details;
+}
+
+std::string CaseOutput(CaseSolution* solution, int case_num)
 {
   std::stringstream ss;
   //ss << "Case #" << case_num << ": " << std::endl;
   return std::string(ss.str());
 }
 
-void OutputSolution(CaseSolution solution, std::ostream& out, int case_num) {
-    out << CaseOutput(solution, case_num);
-}
-
-CaseSolution SolveCase(CaseDetails details, std::string sCase) {
+CaseSolution* SolveCase(CaseDetails* details, std::string sCase) {
     LOG("SolveCase: " << sCase);
-    CaseSolution solution;
+    CaseSolution* solution = new CaseSolution();
 
     return solution;
 }
@@ -107,6 +67,17 @@ CaseSolution SolveCase(CaseDetails details, std::string sCase) {
 bool OutputCompare(std::string mine, std::string ans)
 {
   return false;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// For Incomplete Type
+
+void delete_impl(CaseDetails* details, CaseSolution* solution)
+{
+  delete details;
+  delete solution;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- src/main_standard.h
+++ src/main_standard.h
@@ -31,13 +31,56 @@ bool OpenTest(std::string& name, std::ifstream& fin)
 struct CaseDetails;
 struct CaseSolution;
 
-void SolveProblem(std::istream& in, std::ostream& out);
-void AnalyzeProblem(std::istream& in, std::istream& answers);
-CaseDetails ReadCaseDetails(std::istream& in);
-CaseSolution SolveCase(CaseDetails details, std::string sCase);
-std::string CaseOutput(CaseSolution solution, int case_num);
-void OutputSolution(CaseSolution solution, std::ostream& out, int case_num);
+CaseDetails* ReadCaseDetails(std::istream& in);
+CaseSolution* SolveCase(CaseDetails* details, std::string sCase);
+std::string CaseOutput(CaseSolution* solution, int case_num);
 bool OutputCompare(std::string mine, std::string ans);
+void delete_impl(CaseDetails* details, CaseSolution* solution);
+
+void OutputSolution(CaseSolution* solution, std::ostream& out, int case_num) {
+    out << CaseOutput(solution, case_num);
+}
+
+void SolveProblem(std::istream& in, std::ostream& out) {
+    int num_problems = 0;
+    in >> num_problems;
+
+    for(int i=0; i<num_problems; i++) {
+        CaseDetails* case_details = ReadCaseDetails(in);
+        CaseSolution* solution = SolveCase(case_details, std::to_string(i+1));
+        OutputSolution(solution, out, i+1);
+
+        delete_impl(case_details, solution);
+    }
+}
+
+void AnalyzeProblem(std::istream& in, std::istream& answers) {
+    int num_problems = 0;
+    in >> num_problems;
+
+    LOG("ANALYZING " << num_problems << " problems");
+
+    for(int i=0; i<num_problems; i++) {
+        CaseDetails* case_details = ReadCaseDetails(in);
+        CaseSolution* solution = SolveCase(case_details, std::to_string(i+1));
+
+        std::string ans = CaseOutput(solution, i+1);
+        ans.pop_back(); // Remove newline for logging.comparing
+
+        std::string key;
+        std::getline(answers, key);
+
+        if(!OutputCompare(ans, key))
+        {
+          LOG("MISMATCH (mine, key)");
+          LOG(ans);
+          LOG(key);
+          LOG("");
+        }
+
+        delete_impl(case_details, solution);
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
